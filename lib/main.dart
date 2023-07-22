9477463bd0d5e1db43aa344bffdce8701c1b8a3c
Erenalp <erenalp.kesici@pobol.com.tr>
2023-07-23 01:38:13 +0300

    fixes

--- lib/main.dart
+++ lib/main.dart
@@ -5,26 +5,75 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
-import 'package:mic_stream/mic_stream.dart';
 import 'package:timezone/data/latest.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
+import 'package:noise_meter/noise_meter.dart';
 
 Color color = Colors.white;
 Stream<Uint8List>? stream;
 StreamSubscription<List<int>>? listener;
+
+NoiseReading? _latestReading;
+StreamSubscription<NoiseReading>? _noiseSubscription;
+NoiseMeter? _noiseMeter = NoiseMeter();
+
+void start() {
+  print("A");
+  try {
+    _noiseSubscription = _noiseMeter?.noise.listen(onData);
+  } catch (err) {
+    print('err');
+  }
+}
+
+void onData(NoiseReading noiseReading) {
+  double? db = noiseReading.meanDecibel;
+  if(noiseReading.meanDecibel > 80) {
+
+      AwesomeNotifications().createNotification(
+        content: NotificationContent(
+          id: 0,
+          channelKey: 'basic_channel',
+          title: db.toString() + ' at: ',
+          body: DateTime.now().toString(),
+          wakeUpScreen: true
+        )
+      );
+    }
+  // this.setState(() {
+  //   _latestReading = noiseReading;
+  //   if (!this._isRecording) this._isRecording = true;
+  // });
+}
+// void onError(Object error) {
+//   print(error);
+//   _isRecording = false;
+// }
+// void stop() {
+//   try {
+//     _noiseSubscription?.cancel();
+//     this.setState(() {
+//       this._isRecording = false;
+//     });
+//   } catch (err) {
+//     print(err);
+//   }
+// }
+
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   AwesomeNotifications().initialize(
-    'resource://raw/sound',
-    [
-      NotificationChannel(
-        channelKey: 'basic_channel',
-        channelName: 'Basic notifications',
-        channelDescription: 'Notification channel for basic tests',
-        ledColor: Colors.white,
-        importance: NotificationImportance.Low,
-      ),
-    ]
+      'resource://raw/sound',
+      [
+        NotificationChannel(
+          channelKey: 'basic_channel',
+          channelName: 'Basic notifications',
+          channelDescription: 'Notification channel for basic tests',
+          ledColor: Colors.white,
+          importance: NotificationImportance.Low,
+        ),
+      ]
   );
   runApp(const MyApp());
 }
@@ -42,28 +91,27 @@ class MyApp extends StatelessWidget {
   }
 }
 
-void _startListening(int activeFor) async{
-  print("in");
-  stream = await MicStream.microphone();
-  listener = stream!.listen((samples){
-    print(samples.first);
-    if(samples.first > 130) {
-      AwesomeNotifications().createNotification(
-        content: NotificationContent(
-          id: 0,
-          channelKey: 'basic_channel',
-          title: 'Loud at:',
-          body: DateTime.now().toString(),
-          wakeUpScreen: true
-        )
-      );
-    }
-  });
-  Future.delayed(Duration(minutes: activeFor),(){
-    print("CANcel");
-    listener?.cancel();
-  });
-}
+// void _startListening(int activeFor) async{
+//   stream = await MicStream.microphone();
+//   listener = stream!.listen((samples){
+//     if(samples.first > 130) {
+//     print(samples.first);
+//       AwesomeNotifications().createNotification(
+//         content: NotificationContent(
+//           id: 0,
+//           channelKey: 'basic_channel',
+//           title: samples.first.toString() + ' at: ',
+//           body: DateTime.now().toString(),
+//           wakeUpScreen: true
+//         )
+//       );
+//     }
+//   });
+//   Future.delayed(Duration(minutes: activeFor),(){
+//     print("CANcel");
+//     listener?.cancel();
+//   });
+// }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
@@ -74,7 +122,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   TimeOfDay selectedTime = TimeOfDay.now();
-  TextEditingController tecActiveFor = TextEditingController(text: '10');
+  TextEditingController tecActiveFor = TextEditingController(text: '60');
   TimeOfDay? timeSelected, shownTime;
   @override
   Widget build(BuildContext context) {
@@ -84,7 +132,7 @@ class _MyHomePageState extends State<MyHomePage> {
         actions: [
           IconButton(onPressed: (){
             if(listener != null)listener?.cancel();
-          }, icon: Icon(Icons.stop))
+          }, icon: const Icon(Icons.stop))
         ],
       ),
       body: Center(
@@ -113,19 +161,31 @@ class _MyHomePageState extends State<MyHomePage> {
               ),
             ),
             IconButton(onPressed: () async {
+  print('a');
+              start();
+              return;
               if(listener != null)listener?.cancel();
-              if(timeSelected == null) {
-                _startListening(int.parse(tecActiveFor.text));
+              if(timeSelected == null) {           
+                // _startListening(int.parse(tecActiveFor.text));
+                start();
                 ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Started...")));
               }
-              else{ 
+              else{
                 TimeOfDay now = TimeOfDay.now();
                 int hourTill = timeSelected!.hour - now.hour;
                 int minTill = timeSelected!.minute - now.minute + hourTill*60;
-                ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Min left: " + minTill.toString())));
-                await Future.delayed(Duration(minutes: minTill), () async{
-                  _startListening(int.parse(tecActiveFor.text));
-                });
+                if(minTill > 0){
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Min left: " + minTill.toString())));
+                  await Future.delayed(Duration(minutes: minTill), () async{
+                    // _startListening(int.parse(tecActiveFor.text));
+                    start();
+                  });
+                }
+                else{
+                  start();
+                  // _startListening(int.parse(tecActiveFor.text));
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text("Started...")));
+                }
               }
             }, icon: const Icon(Icons.task_alt))
           ],
